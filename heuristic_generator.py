ff701d19bf9d35ede5b1311c948e2447fc2fbcb1
Paroma Varma <varma.paroma@gmail.com>
2017-10-05 17:10:05 -0700

    comments, beta and gamma

--- heuristic_generator.py
+++ heuristic_generator.py
@@ -9,18 +9,34 @@ class HeuristicGenerator(object):
     """
 
     def __init__(self, train_primitive_matrix, val_primitive_matrix, 
-    val_ground, train_ground=None, b=0.5, hf_cutoff=0.2, fb_thresh=):
-    #TODO: add option for existing heuristics and/or L_train/L_val
+    val_ground, train_ground=None, b=0.5, beta=0.2, gamma=0.35):
+        """ 
+        Initialize HeuristicGenerator object
+
+        b: class prior of most likely class (TODO: use somewhere)
+        beta: threshold to decide whether to abstain or label for heuristics
+        gamma: threshold to decide whether to call a point vague or not
+        """
+
         self.train_primitive_matrix = train_primitive_matrix
         self.val_primitive_matrix = val_primitive_matrix
         self.val_ground = val_ground
         self.train_ground = train_ground
         self.b = b
+        self.beta = beta
+        self.gamma = gamma
 
         self.hf = []
         self.feat_combos = []
 
     def run_synthesizer(self, cardinality=1, idx=None, keep=1):
+        """ 
+        Generates Synthesizer object and saves all generated heuristics
+
+        cardinality: number of features candidate programs take as input
+        idx: indices of validation set to fit programs over
+        keep: number of heuristics to pass to verifier
+        """
         if idx == None:
             primitive_matrix = self.val_primitive_matrix
             ground = self.val_ground
@@ -42,6 +58,9 @@ class HeuristicGenerator(object):
         self.L_val = self.syn.apply_heuristics(self.hf,self.X_val)
 
     def evaluate(self):
+        """ 
+        Calculate the accuracy and coverage for train and validation sets
+        """
         self.val_marginals = self.vf.val_marginals
         self.train_marginals = self.vf.train_marginals
 
@@ -65,13 +84,20 @@ class HeuristicGenerator(object):
         return self.val_accuracy, self.train_accuracy, self.val_coverage, self.train_coverage
     
     def run_verifier(self):
+        """ 
+        Generates Verifier object and saves marginals
+        """
         self.vf = Verifier(self.L_train, self.L_val, self.val_ground)
         self.vf.train_gen_model()
         self.vf.assign_marginals()
 
     def find_feedback(self):
-        print 'In fb ', np.shape(self.vf.L_train)
-        vague_idx = self.vf.find_vague_points(b=self.b, thresh=self.thresh)
+        """ 
+        Finds vague points according to gamma parameter
+
+        self.gamma: confidence past 0.5 that relates to a vague or incorrect point
+        """
+        vague_idx = self.vf.find_vague_points(b=self.b, thresh=self.gamma)
         #TODO: flag for re-classifying incorrect points
         #incorrect_idx = self.vf.find_incorrect_points(b=self.b)
         incorrect_idx = vague_idx
