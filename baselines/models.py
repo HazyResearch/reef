01d726e48236c12db7e3be78cf5a26460b482675
Paroma Varma <varma.paroma@gmail.com>
2017-10-10 20:58:54 -0700

    added semisupervised methods

--- baselines/models.py
+++ baselines/models.py
@@ -1,6 +1,16 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.semi_supervised import LabelSpreading
+
+#need to clone https://github.com/tmadl/semisup-learn
+#TODO: work in progress, waiting on Cmake
+# import os
+# import sys
+# sys.path.append('/dfs/scratch0/paroma/semisup-learn/')
+# sys.path.append('/dfs/scratch0/paroma/nlopt-2.4.2/')
+# sys.path.append('/dfs/scratch0/paroma/include/')
+# from frameworks.CPLELearning import CPLELearningModel
 
 
 class BaselineModel(object):
@@ -42,7 +52,6 @@ class BaselineModel(object):
             total = np.shape(np.where(marginals != 0.5))[1]
             labels = np.sign(2*(marginals - 0.5))
             return total/float(len(labels))
-
         
         self.val_accuracy = calculate_accuracy(self.val_marginals, self.b, self.val_ground)
         self.train_accuracy = calculate_accuracy(self.train_marginals, self.b, self.train_ground)
@@ -68,3 +77,38 @@ class DecisionTree(BaselineModel):
     def fit(self):
         self.model = DecisionTreeClassifier(random_state=0)
         self.model.fit(self.val_primitive_matrix, self.val_ground)
+
+class SemiSupervised(BaselineModel):
+    """
+    LabelSpreading Implementation
+    """
+
+    def fit(self):
+        #Need to concatenate labeled and unlabeled data
+        #unlabeled data labels are set to -1
+        X = np.concatenate((self.val_primitive_matrix, self.train_primitive_matrix))
+        val_labels = (self.val_ground+1)/2.
+        train_labels = -1.*np.ones(np.shape(self.train_ground))
+        y = np.concatenate((val_labels, train_labels))
+
+        self.model = LabelSpreading(kernel='knn')
+        self.model.fit(X, y)
+
+class ContrastiveSemiSupervised(BaselineModel):
+    """
+    Contrastive Pessimistic Likelihood Estimation Implementation
+    """
+
+    def fit(self):
+        pass
+        #Need to concatenate labeled and unlabeled data
+        #unlabeled data labels are set to -1
+        X = np.concatenate((self.val_primitive_matrix, self.train_primitive_matrix))
+        val_labels = (self.val_ground+1)/2.
+        train_labels = -1.*np.ones(np.shape(self.train_ground))
+        y = np.concatenate((val_labels, train_labels))
+
+        self.model = CPLELearningModel()
+        self.model.fit(X, y)
+
+
